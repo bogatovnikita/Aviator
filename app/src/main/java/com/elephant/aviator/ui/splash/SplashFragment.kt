fb4c874e4f9f02d7c6cdb07d023f196f8aee5f5f
Nikita Bogatov <bogatovnikita@gmail.com>
2022-09-26 17:08:20 +0300

    add navigation and anim for splash

--- app/src/main/java/com/elephant/aviator/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/elephant/aviator/ui/splash/SplashFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.elephant.aviator.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
@@ -18,6 +22,16 @@ class SplashFragment : Fragment() {
     ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(4000L)
+            findNavController().navigate(
+                SplashFragmentDirections.actionSplashFragmentToListOfAircraftFragment()
+            )
+        }
     }
 
     override fun onDestroy() {
